85b0abf2c3158fba3bd18747c3204104c340a6fb
artemtelizhenko <artem.telizhenko@refinitiv.com>
2022-06-25 20:40:29 +0300

    adds fizz buzz task

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Solved in java lang most popular tasks, such as:
  1. Palindrom Linked List
  2. Roman to Integer
  3. Ransom Note
+ 4. Fizz Buzz
---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int n = 15;
+        System.out.printf("Input: n = %d%n", n);
+        System.out.printf("Output: %s%n", fizzBuzz.fizzBuzz(n));
+    }
+    public List<String> fizzBuzz(int n) {
+        List<String> fizzBuzzList = new ArrayList<>(n);
+
+        for (int i = 1; i <= n; i++) {
+             if (i % 3 == 0 && i % 5 == 0) {
+                 fizzBuzzList.add("FizzBuzz");
+             } else if (i % 3 == 0) {
+                 fizzBuzzList.add("Fizz");
+             } else if (i % 5 == 0) {
+                 fizzBuzzList.add("Buzz");
+             } else {
+                 fizzBuzzList.add(String.valueOf(i));
+             }
+
+        }
+
+        return fizzBuzzList;
+    }
+}
